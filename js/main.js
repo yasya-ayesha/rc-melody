52155f478fa5cf30cf7f7528c85ac6baa9f42eec
Yasya <80125891+yasya-ayesha@users.noreply.github.com>
2021-08-25 15:09:05 +0300

    Add files via upload
    add floor plan, make interactive flats choosing

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 /*jshint esversion: 6 */ 
 $(document).ready(function () {
     let currentFloor = 2;
+    let currentFlat = 40;
     const floorPath = $('.home-image path');
     const counterUp = $('.counter-up');
     const counterDown = $('.counter-down');
+    const flatPath = $('.flats path');
+    const flatLink = $('.flat-link');
+    const modal = $('.modal');
+    const modalCloseButton = $('.modal-close-button');
+    const viewFlats = $('.view-flats');
     
     floorPath.on('mouseover', function () {
         floorPath.removeClass('current-floor');
         currentFloor = $(this).attr('data-floor');
         $('.counter').text(currentFloor);
     });
+
+    floorPath.on('click', toggleModal);
+    viewFlats.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
 
     counterUp.on('click', function () {
         if (currentFloor < 18) {
@@ -30,4 +40,18 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
     });
+
+    flatPath.on('mouseover', dataFlat);
+    flatLink.on('mouseover', dataFlat);
+
+    function toggleModal() {
+        modal.toggleClass('is-open');
+    }
+
+    function dataFlat() {
+        flatPath.removeClass('current-flat');
+        flatLink.removeClass('current-flat');
+        currentFlat = $(this).attr('data-flat');
+        $(`[data-flat=${currentFlat}]`).toggleClass('current-flat');
+    }
 });
